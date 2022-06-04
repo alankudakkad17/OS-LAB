a669ecb322adacb940942be970f609de7487f939
ALAN ANTO <89739433+alankudakkad17@users.noreply.github.com>
2022-06-04 10:43:12 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,15 @@
+echo "Enter the name"
+read name
+echo "Enter the semester"
+read sem
+echo "Enter the batch"
+read batch
+echo "Enter the roll_number"
+read roll
+echo "Enter the register number"
+read reg_no
+echo "Name:" $name
+echo "Semester:" $sem
+echo "Batch:" $batch
+echo "roll_number" $roll
+echo "Register number:" $reg_no
